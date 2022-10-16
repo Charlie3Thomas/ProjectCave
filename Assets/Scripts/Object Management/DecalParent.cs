8cd11a7dfafd25c80485aad966dd682ed408e109
Charlie <charlie3thomas@live.uwe.ac.uk>
2022-10-16 15:20:29 +0100

    [ProjectCave] Minor tweaks

--- Assets/Scripts/Object Management/DecalParent.cs
+++ Assets/Scripts/Object Management/DecalParent.cs
@@ -5,41 +5,30 @@ using UnityEngine.Assertions.Must;
 
 public class DecalParent : MonoBehaviour
 {
-    private bool mr_enabled = true;
-    private bool last_update = true;
+    [SerializeField] private float t_updaterate = 1.0f;
     private GameObject go_player;
+    private GameObject go_parent;
     private float t;
-    private float t_updaterate;
 
     private void Update()
     {
-        //this.GetComponent<MeshRenderer>().enabled = (go_player.transform.position - this.transform.position).sqrMagnitude < 300.0f;
-
         t += Time.deltaTime;
-        if (t > 0.5)
+        RenderState();
+    }
+
+    private void RenderState()
+    {
+        if (t > t_updaterate)
         {
             this.GetComponent<MeshRenderer>().enabled = (go_player.transform.position - this.transform.position).sqrMagnitude < 1000.0f;
             t = 0.0f;
         }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            mr_enabled = !mr_enabled;
-        }
-
-        if (last_update != mr_enabled)
-        {
-            this.GetComponent<MeshRenderer>().enabled = mr_enabled;
-
-        }
-        last_update = mr_enabled;
-
-
-
     }
+
     private void OnEnable()
     {
-        this.transform.parent = GameObject.FindGameObjectWithTag("GameController").transform;
         go_player = GameObject.FindGameObjectWithTag("Player");
+        go_parent = GameObject.FindGameObjectWithTag("GameController");
+        this.transform.parent = go_parent.transform;
     }
 }
